e822f07d63b37ad561d597765edb7e2aa8df6118
souravcrj5495 <souravcrj1995@gmail.com>
2023-07-10 00:55:18 +0530

    Lets Scale the app to more users

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 const addInput = ()=>{
-localStorage.clear();
 
 //taking the input values     
 let name = document.getElementById('name').value;
 let email = document.getElementById('email').value;
-
+let key = "user_" + email;
 //Storing in local storage in object form
 let myObj = {
     name: name,
     email: email
 };
-localStorage.setItem("UserDetails", JSON.stringify(myObj));
+localStorage.setItem(key,JSON.stringify(myObj));
 alert('form data saved !');
+
 }
